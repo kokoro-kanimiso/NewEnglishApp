f045d49b806c84a045ff043f9d8d573dabae5029
kanimiso <kokorohamada1226@gmail.com>
2024-05-12 00:26:16 +0900

    Login function completed

--- english-app/app/common/Title.tsx
+++ english-app/app/common/Title.tsx
@@ -4,8 +4,7 @@ export const StyledTitle = styled.h2`
   color: #bb86fc;
   font-size: 5rem;
   text-align: center;
-  border-bottom: 1px solid black;
-  padding-bottom: 30px;
+  padding-bottom: 10px;
 `;
 
 const Title = () => {};
